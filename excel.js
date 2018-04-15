ddbb7d68c48fb9c5f7e7f100b0e17585663ef0f6
Eric <coolspot1000@gmail.com>
2018-04-14 22:04:50 -0500

    Download Configured

--- excel.js
+++ excel.js
@@ -2,49 +2,13 @@ var xl = require('excel4node');
 var Project = require('./models/project');
 var User = require('./models/user');
 
-// Create a new instance of a Workbook class
-module.exports.createWorkbook = function () {
-
-
-    var wb = new xl.Workbook();
-
-    // Add Worksheets to the workbook
-    var ws = wb.addWorksheet('Sheet 1');
-    var ws2 = wb.addWorksheet('Sheet 2');
-
-    // Create a reusable style
-    var style = wb.createStyle({
-        font: {
-            color: '#FF0800',
-            size: 12
-        },
-        numberFormat: '$#,##0.00; ($#,##0.00); -'
-    });
-
-    // Set value of cell A1 to 100 as a number type styled with paramaters of style
-    ws.cell(1, 1).number(100).style(style);
-
-    // Set value of cell B1 to 300 as a number type styled with paramaters of style
-    ws.cell(1, 2).number(200).style(style);
-
-    // Set value of cell C1 to a formula styled with paramaters of style
-    ws.cell(1, 3).formula('A1 + B1').style(style);
-
-    // Set value of cell A2 to 'string' styled with paramaters of style
-    ws.cell(2, 1).string('string').style(style);
-
-    // Set value of cell A3 to true as a boolean type styled with paramaters of style but with an adjustment to the font size.
-    ws.cell(3, 1).bool(true).style(style).style({
-        font: {
-            size: 14
-        }
-    });
-
-    wb.write('Excel.xlsx');
-}
-
 function buildSliderName(standard,member){
     return String(standard + "slider" + member);
+}
+
+function buildFileName(title){
+    var d = new Date();
+    return String(title+"_"+ (d.getMonth()+1) + d.getDate()+'.xlsx');
 }
 module.exports.exportResults = function (id) {
     Project.findOne({
@@ -93,16 +57,17 @@ module.exports.exportResults = function (id) {
                         ws.cell(math, 2).string(String(groupMemberArray[i][z])).style(style);
                         var slider = buildSliderName(0, z);
                         for(var y=0; y<numStandards; y++){
-                            ws.cell(math, y+3).number(parseInt(projectData[i][buildSliderName(y,z)])).style(style);
+                            ws.cell(math, y+3).number(Number(projectData[i][buildSliderName(y,z)])).style(style);
                         }
                         
                     }
 
                 }
             }
-
-            console.log("Writing File");
-            wb.write('Results.xlsx');
+            process.chdir('../EasyEval/Exports');
+            console.log("Writing File to " + process.cwd());
+            wb.write(buildFileName(project.projectTitle));
+            process.chdir('./');
         });
     });
 
